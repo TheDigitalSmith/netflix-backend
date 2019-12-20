0a40912c70f8e40a98fa2c2195705328c04b0003
infinityshades <tash.aneiq@gmail.com>
2019-12-20 11:50:03 +0100

    film crud with media upload done

--- services/media/index.js
+++ services/media/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router();
 const {getFilms, writeFilms,} = require("../data/dataHelper")
+const fs = require("fs-extra");
+const path = require("path")
+const multer = require("multer");
 
 const uuidv4 = require("uuid/v4")
 
@@ -22,6 +25,24 @@ router.post("/", async(req,res)=>{
     films.push(newFilm);
     await writeFilms(films);
     res.send(newFilm)
+})
+
+//UPLOADING MEDIA
+const multerConfig = multer({});
+
+router.post("/:id/upload",multerConfig.single("filmPic"),async (req,res)=>{
+    const films = await getFilms();
+    const film = films.find(film => film._id === req.params.id)
+    if(film){
+        const filedest = path.join(__dirname,"../../images", req.params.id + path.extname(req.file.originalname))
+        await fs.writeFile(filedest, req.file.buffer)
+        film.updatedAt = new Date();
+        film.poster = "/images/" + req.params.id + path.extname(req.file.originalname);
+        await writeFilms(film);
+        res.send(film)
+    }else{
+        res.status("404").send("film not found")
+    }
 })
 
 //DELETING FILM
